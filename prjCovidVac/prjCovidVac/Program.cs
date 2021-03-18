32fb6387413d6f30092b3c3c798832fcc4da8d42
JarrydClaasen <80829110+JarrydClaasen@users.noreply.github.com>
2021-03-18 10:41:27 +0200

    Add files via upload

--- prjCovidVac/prjCovidVac/Program.cs
+++ prjCovidVac/prjCovidVac/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace prjCovidVac
 {
@@ -6,7 +6,29 @@ namespace prjCovidVac
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Information i = new Information();
+
+            Console.Write("Please enter the hospital's name:");
+            i.setHospital_Name(Console.ReadLine());
+
+            Console.Write("Please enter if the hospital is Public or Private: ");
+            i.setHospitalType(Console.ReadLine());
+
+            Console.Write("Please enter the address: ");
+            i.setAddress(Console.ReadLine());
+
+            Console.Write("Please enter the hospital's province :");
+            i.setProvince(Console.ReadLine());
+
+            Console.Write("Please enter the hospital's province :");
+            i.setArray(Convert.ToChar(Console.ReadLine()));
+
+            Capture c = new Capture();
+
+            c.GetPatientInformation();
+
+
         }
     }
 }
+
